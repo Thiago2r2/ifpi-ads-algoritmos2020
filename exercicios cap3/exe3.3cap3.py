82e06b4e79eba2b3ede05608432fee8b90644a66
proview2r <thiago2rnexus@gmail.com>
2020-03-10 23:21:10 -0300

    exercicios cap 3

---
+++ exercicios cap3/exe3.3cap3.py
@@ -0,0 +1,18 @@
+def print1(x):
+    print("+" + 4*" - " + "+" + 4*" - "+"+")
+    
+print1(print1)
+
+def print2(y):
+    print("|"+ 12*" "+"|"+ 12*" "+"|")
+    
+print2(print2)
+print2(print2)
+print2(print2)
+print2(print2) 
+print1(print1)
+print2(print2) 
+print2(print2) 
+print2(print2) 
+print2(print2) 
+print1(print1)
